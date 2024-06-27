77da6d09adf59bd2088fe64d6844c76dc84a50fe
Aiman Naeem <109482563+Aimancod01@users.noreply.github.com>
2024-06-27 11:09:49 +0500

    first commit

---
+++ src/pages/Main.jsx
@@ -0,0 +1,35 @@
+import car from "../assets/images/car.jpg";
+import tour from "../assets/images/tour.jpg";
+import roadside from "../assets/images/roadside.jpg";
+function Main() {
+  return (
+    <div className="px-10 py-12 ">
+      <div className="flex justify-between space-x-10">
+        <div className="relative pl-2  ">
+          <img src={car} alt="" className="h-96 rounded-lg w-96" />
+          <h1 className="absolute top-0 pl-20 pt-12 text-white rounded-lg hover:scale-105 transition-all text-2xl font-bold h-96 w-full bg-black bg-opacity-70 ">
+            Car Rental Service
+          </h1>
+        </div>
+        <div className="relative">
+          <img src={tour} alt="" className="h-96 rounded-lg w-96 " />
+          <h1 className="absolute top-0 pl-24 pt-12 text-white rounded-lg hover:scale-105 transition-all text-2xl font-bold h-96 w-full bg-black bg-opacity-70 ">
+            Tourism Service
+          </h1>
+        </div>
+        <div className="relative">
+          <img
+            src={roadside}
+            alt=""
+            className="h-96 rounded-lg shadow-lg w-96 "
+          />
+          <h1 className="absolute top-0 pl-20 pt-12 text-white rounded-lg hover:scale-105 transition-all text-2xl font-bold h-96 w-full bg-black bg-opacity-70 ">
+            Roadside Assistance
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
